81ff998802c33293774415c8536d0f28ca455350
Aurelien Tavernier <aurelientav@gmail.com>
2024-12-05 20:48:08 +0100

    big update on spawn, now works on every terrain
    Next update will use every collider

--- Assets/Script/Sledge/S_Terrain.cs
+++ Assets/Script/Sledge/S_Terrain.cs
@@ -3,4 +3,10 @@ using UnityEngine;
 public class S_Terrain : MonoBehaviour
 {
     public Transform NewTerrainPos;
+
+    private void Awake()
+    {
+        Vector3 terrainSize = GetComponent<MeshCollider>().bounds.size;
+        NewTerrainPos.position = transform.localPosition - new Vector3(terrainSize.x, terrainSize.y, 0);
+    }
 }
